b1b37d360e53f1a769bb6a76f1d79058724c1dd5
Leah Perlmutter <leahp62@gmail.com>
2019-07-10 12:10:31 -0700

    practice accordion works

--- appside/index.js
+++ appside/index.js
@@ -1,7 +1,23 @@
 "use strict";
 
-let data = knowledgebase_data;
+let data = KNOWLEDGEBASE_DATA;
 console.log(data);
+
+// display the anger data sheet
+// first, filter data to get only anger bullets
+
+let anger_data = [];
+for (let item of data) {
+    if (item.Emotion === 'Anger') {
+        anger_data.push(item);
+    }
+}
+
+console.log(anger_data);
+
+// Display it in the dom with headers for categories and list items under them for statements
+
+
 
 
 
