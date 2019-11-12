7ad2fc8ca18dbea603263243f8a06cb7ce18f88c
Rachel <sittr@cs.washington.edu>
2019-11-12 13:14:13 -0800

    removed button in likertFrame

--- appside/frames/likert.js
+++ appside/frames/likert.js
@@ -17,18 +17,11 @@ class LikertFrame extends Frame {
      * Construct LikertFrame from an object
      * 
      * @param frame_data -- Object containing the frame's data. Expected fields:
-     *    frame_data.template -- The exact string 'statements'
-     *    frame_data.title (string) -- The frame's title
-     *    frame_data.instruction (string) -- Instructions for user
      *    frame_data.question (string) -- Text to appear before the list of statements
      *  Behavior undefined if frame does not have these properties.
      */
     constructor(frame_data) {
         super();
-
-        // check validity (todo)
-        // has title with length > 1
-        // has question with length > 1
 
         // set fields
         this.title = LIKERT_TITLE;
@@ -56,12 +49,6 @@ class LikertFrame extends Frame {
         $(title).text(this.title);
         $(title).attr('class', 'text-info text-uppercase mb-2');
         frame.appendChild(title);
-
-        // insert a h2 node for the question
-        let question = document.createElement('h2');
-        $(question).text(this.question);
-        $(question).attr('class', 'font-weight-light mb-4');
-        frame.appendChild(question);
         
         // insert a h5 node for the instruction
         let instructions = document.createElement('h5');
@@ -91,7 +78,6 @@ class LikertFrame extends Frame {
                 $(input).attr('class', 'likert_input');
                 $(input).attr('type', 'radio');
                 $(input).attr('name', question);    // question text
-                $(input).prop('checked', answer);
                 input.dataset.text = j;             // answer choice
 
                 let input_text = document.createElement('p');
@@ -104,14 +90,6 @@ class LikertFrame extends Frame {
             statements.appendChild(document.createElement('br'));
         }
         frame.appendChild(statements);
-
-        let next = document.createElement('button');
-        $(next).attr('class', 'bodymap_button');
-        $(next).text('Next');
-        $(next).click(function() {
-            console.log(this.get_user_input());
-        }.bind(this));
-        statements.appendChild(next);
 
         let old_frame = $('#frame')[0];
         old_frame.replaceWith(frame);
