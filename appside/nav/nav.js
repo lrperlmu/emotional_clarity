9d9866ca4dd511bb2623d144acccd9835d147364
Leah Perlmutter <leahp62@gmail.com>
2019-12-04 10:25:05 -0800

    move frame factory to frame.js

--- appside/nav/nav.js
+++ appside/nav/nav.js
@@ -92,30 +92,3 @@ class Nav {
         this.render();
     }
 }
-
-/**
- * Factory to build various kinds of frame objects polymorphically
- */
-class FrameFactory {
-    /**
-     * Take in a frame template and return the correct type of Frame object,
-     * initialized with the given template field from frame.
-     */
-    static build(frame) {
-        if(frame.template === INTRO_FRAME_TEMPLATE) {
-            return new IntroFrame(frame);
-        } else if(frame.template === STATEMENTS_FRAME_TEMPLATE) {
-            return new StatementsBodyFrame(frame);
-        } else if(frame.template === SUMMARY_COUNT_FRAME_TEMPLATE) {
-            return new SummaryFrameCount(frame);
-        } else if(frame.template === LIKERT_FRAME_TEMPLATE) {
-            return new LikertFrame(frame);
-        } else if(frame.template === SELF_REPORT_FRAME_TEMPLATE) {
-            return new SelfReportFrame(frame);
-        } else if(frame.template === CONSENT_FRAME_TEMPLATE) {
-            return new ConsentDisclosureFrame(frame);
-        } else {
-            throw new Error('Frame template not recognized ' + frame.template);
-        }
-    }
-}
