af2adf60ed873b9868ffcf4e95097dab374dd73b
Leah Perlmutter <leahp62@gmail.com>
2020-01-10 11:11:02 -0800

    code so that userdata issue causes error

--- appside/nav/test_nav.js
+++ appside/nav/test_nav.js
@@ -2,7 +2,13 @@
 
 $(document).ready(function() {
     let knowledgebase = KNOWLEDGEBASE_DATA;
-    let config = FWD_PROMPTING_CONFIG;
+
+    //let config = FWD_PROMPTING_CONFIG;
+    let config = new DbtWorksheetModelConfig(DIRECTION_FWD, SECTION_PROMPTING);
+    config.set_self_report(true);
+    //config.set_pre_post_measurement(true);
+    //config.set_consent_disclosure(true);
+
     let model = new DbtWorksheetModelFwd(knowledgebase, config);
     let nav = new Nav(model);
     nav.render();
